cdcde8c8bd313724d446ac22237228aea8e386b3
Eric Fleming <eric-fleming18@hotmail.com>
2013-12-19 09:31:34 -0500

    Green - Added first test

---
+++ FizzBuzz/FizzBuzz/Unit Tests/IsDivisibleByThreeShould.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz.Unit_Tests
+{
+    [TestFixture]
+    class IsDivisibleByThreeShould
+    {
+        [Test]
+        public void ReturnTrueIfNumberIsDivisibleByThree()
+        {
+            var fizzBuzz = new FizzBuzz();
+            var expectedValue = true;
+
+            var actualValue = fizzBuzz.IsDivisibleByThree();
+
+            Assert.AreEqual(expectedValue, actualValue);
+
+        }
+    }
+}
